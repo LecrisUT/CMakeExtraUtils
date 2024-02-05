d412a6a709fdcc5eba63b9b868502b7ee35baa01
Cristian Le <git@lecris.dev>
2024-02-05 13:40:52 +0100

    fix: git describe fallback with no tag (#34)
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- test/DynamicVersion/test_fallback.sh
+++ test/DynamicVersion/test_fallback.sh
@@ -8,9 +8,9 @@ rlJournalStart
 		rlRun "rsync -r ./ $tmp" 0 "Copy test files"
 		rlRun "pushd $tmp"
 		rlRun "build_dir=./build" 0 "Set build_dir"
-    rlRun "configure_args=\"-B \${build_dir} -G Ninja --log-context\"" 0 "Set configure_args"
-    rlRun "build_args=\"--build \${build_dir} -v\"" 0 "Set build_args"
-    [[ -n "$CMakeExtraUtils_ROOT" ]] && rlRun "configure_args=\"\${configure_args} -DCMakeExtraUtils_ROOT=\${CMakeExtraUtils_ROOT}\"" 0 "Add CMakeExtraUtils_ROOT"
+		rlRun "configure_args=\"-B \${build_dir} -G Ninja --log-context\"" 0 "Set configure_args"
+		rlRun "build_args=\"--build \${build_dir} -v\"" 0 "Set build_args"
+		[[ -n "$CMakeExtraUtils_ROOT" ]] && rlRun "configure_args=\"\${configure_args} -DCMakeExtraUtils_ROOT=\${CMakeExtraUtils_ROOT}\"" 0 "Add CMakeExtraUtils_ROOT"
 		rlRun "set -o pipefail"
 	rlPhaseEnd
 
@@ -20,8 +20,8 @@ rlJournalStart
 	rlPhaseEnd
 
 	rlPhaseStartTest "With fallback version"
-    [[ -d ${build_dir} ]] && rlRun "rm -rf ${build_dir}" 0 "Clean the build directory"
-	  rlRun "fallback_version='0.1.2'" 0 "Set fallback_version"
+		[[ -d ${build_dir} ]] && rlRun "rm -rf ${build_dir}" 0 "Clean the build directory"
+		rlRun "fallback_version='0.1.2'" 0 "Set fallback_version"
 		rlRun -s "cmake ${configure_args} -DFALLBACK_VERSION=${fallback_version}" 0 "CMake configure"
 		rlAssertGrep "^\[TestProject\] version: ${fallback_version}\$" $rlRun_LOG
 		rlAssertGrep "^\[TestProject\] version-full: ${fallback_version}\$" $rlRun_LOG
@@ -33,18 +33,18 @@ rlJournalStart
 		rlRun -s "${build_dir}/version" 0 "Run ./version"
 		rlAssertGrep "^version: ${fallback_version}\$" $rlRun_LOG
 		rlAssertGrep "^version-full: ${fallback_version}\$" $rlRun_LOG
-    rlRun -s "${build_dir}/commit" 0 "Run ./commit"
-    rlAssertGrep "^version: ${fallback_version}\$" $rlRun_LOG
-    rlAssertGrep "^version-full: ${fallback_version}\$" $rlRun_LOG
-    rlAssertGrep "^commit: commit-NOTFOUND\$" $rlRun_LOG
-    rlAssertGrep "^short-hash: short-hash-NOTFOUND\$" $rlRun_LOG
-    rlAssertGrep "^describe: describe-NOTFOUND\$" $rlRun_LOG
-    rlAssertGrep "^distance: distance-NOTFOUND\$" $rlRun_LOG
+		rlRun -s "${build_dir}/commit" 0 "Run ./commit"
+		rlAssertGrep "^version: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^version-full: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^commit: commit-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^short-hash: short-hash-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^describe: describe-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^distance: distance-NOTFOUND\$" $rlRun_LOG
 	rlPhaseEnd
 
 	rlPhaseStartTest "With fallback version + commit"
-    [[ -d ${build_dir} ]] && rlRun "rm -rf ${build_dir}" 0 "Clean the build directory"
-	  rlRun "fallback_commit='dummy'" 0 "Set fallback_commit"
+		[[ -d ${build_dir} ]] && rlRun "rm -rf ${build_dir}" 0 "Clean the build directory"
+		rlRun "fallback_commit='dummy'" 0 "Set fallback_commit"
 		rlRun -s "cmake ${configure_args} -DFALLBACK_VERSION=${fallback_version} -DFALLBACK_HASH=${fallback_commit}" 0 "CMake configure"
 		rlAssertGrep "^\[TestProject\] version: ${fallback_version}\$" $rlRun_LOG
 		rlAssertGrep "^\[TestProject\] version-full: ${fallback_version}\$" $rlRun_LOG
@@ -56,13 +56,38 @@ rlJournalStart
 		rlRun -s "${build_dir}/version" 0 "Run ./version"
 		rlAssertGrep "^version: ${fallback_version}\$" $rlRun_LOG
 		rlAssertGrep "^version-full: ${fallback_version}\$" $rlRun_LOG
-    rlRun -s "${build_dir}/commit" 0 "Run ./commit"
-    rlAssertGrep "^version: ${fallback_version}\$" $rlRun_LOG
-    rlAssertGrep "^version-full: ${fallback_version}\$" $rlRun_LOG
-    rlAssertGrep "^commit: ${fallback_commit}\$" $rlRun_LOG
-    rlAssertGrep "^short-hash: short-hash-NOTFOUND\$" $rlRun_LOG
-    rlAssertGrep "^describe: describe-NOTFOUND\$" $rlRun_LOG
-    rlAssertGrep "^distance: distance-NOTFOUND\$" $rlRun_LOG
+		rlRun -s "${build_dir}/commit" 0 "Run ./commit"
+		rlAssertGrep "^version: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^version-full: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^commit: ${fallback_commit}\$" $rlRun_LOG
+		rlAssertGrep "^short-hash: short-hash-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^describe: describe-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^distance: distance-NOTFOUND\$" $rlRun_LOG
+	rlPhaseEnd
+
+	rlPhaseStartTest "With fallback version + git repo, no tag"
+		[[ -d ${build_dir} ]] && rlRun "rm -rf ${build_dir}" 0 "Clean the build directory"
+		rlRun "git init"
+		rlRun "git add CMakeLists.txt .git_archival.txt src pyproject.toml" 0 "Git add basic files"
+		rlRun "git commit -m 'Initial commit'" 0 "Git commit (initial)"
+		rlRun -s "cmake ${configure_args} -DFALLBACK_VERSION=${fallback_version}" 0 "CMake configure"
+		rlAssertGrep "^\[TestProject\] version: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^\[TestProject\] version-full: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^\[TestProject\] commit: commit-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^\[TestProject\] short-hash: short-hash-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^\[TestProject\] describe: describe-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^\[TestProject\] distance: distance-NOTFOUND\$" $rlRun_LOG
+		rlRun -s "cmake ${build_args}" 0 "CMake build"
+		rlRun -s "${build_dir}/version" 0 "Run ./version"
+		rlAssertGrep "^version: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^version-full: ${fallback_version}\$" $rlRun_LOG
+		rlRun -s "${build_dir}/commit" 0 "Run ./commit"
+		rlAssertGrep "^version: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^version-full: ${fallback_version}\$" $rlRun_LOG
+		rlAssertGrep "^commit: commit-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^short-hash: short-hash-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^describe: describe-NOTFOUND\$" $rlRun_LOG
+		rlAssertGrep "^distance: distance-NOTFOUND\$" $rlRun_LOG
 	rlPhaseEnd
 
 	rlPhaseStartCleanup
